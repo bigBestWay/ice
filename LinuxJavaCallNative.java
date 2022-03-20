618b6042e8c916af75b11e3f37920108dcf625db
夏天 <47944478+SummerSec@users.noreply.github.com>
2022-03-20 12:22:16 +0800

    修改为 ByteBuffer.allocate(8);
    Long.BYTES 是jdk8之后才有的，Long.BYTES = 8 所以直接修改成8可以获得更好的兼容性。

--- LinuxJavaCallNative.java
+++ LinuxJavaCallNative.java
@@ -186,7 +186,9 @@ public class LinuxJavaCallNative {
 		ret
 		*/
 		byte[] movabs_rax = {0x48, (byte) 0xb8};
-        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+        //  ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+	//  Long.BYTES 是jdk8之后才有的，Long.BYTES = 8 所以直接修改成8可以获得更好的兼容性。
+	ByteBuffer buffer = ByteBuffer.allocate(8);
         buffer.order(ByteOrder.LITTLE_ENDIAN);
         buffer.putLong(0, JNI_GetCreatedJavaVMs);
         
